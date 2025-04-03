91bb833bdb0fd107c253f1a230000832076a9f65
Nilo <nilogabriel21@gmail.com>
2025-04-03 11:05:02 -0300

    Desafio Realizado!

--- DesafioContaBanco/src/ContaTerminal.java
+++ DesafioContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception{
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Digite seu nome: ");
+        String nome = sc.next(); 
+        sc.nextLine();
+        System.out.print("Digite seu sobrenome: ");
+        String Sobrenome = sc.next(); 
+        sc.nextLine();
+        String NomeCLiente = nome.concat(" ").concat(Sobrenome); 
+        System.out.print("Digite sua agência: ");
+        String Agencia = sc.next();
+        System.out.print("Digite o número da sua agência: ");
+        int Numero = sc.nextInt();
+        System.out.print("Digite seu saldo: ");
+        double Saldo = sc.nextDouble(); 
+
+        String texto = String.format("%nOlá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.%n",NomeCLiente,Agencia,Numero,Saldo);
+        System.out.println(texto);
+
+        sc.close();
     }
 }
