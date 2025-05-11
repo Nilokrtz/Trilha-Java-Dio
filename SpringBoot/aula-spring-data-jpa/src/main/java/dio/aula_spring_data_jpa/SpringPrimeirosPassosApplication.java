bf534eb66f3c17ee9db219a4be997feb7f2dcdcf
Nilo <nilogabriel21@gmail.com>
2025-05-11 16:56:20 -0300

    Mexendo com o banco de Dados através de JPA

---
+++ SpringBoot/aula-spring-data-jpa/src/main/java/dio/aula_spring_data_jpa/SpringPrimeirosPassosApplication.java
@@ -0,0 +1,13 @@
+package dio.aula_spring_data_jpa;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrimeirosPassosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPrimeirosPassosApplication.class, args);
+	}
+	
+
+}
